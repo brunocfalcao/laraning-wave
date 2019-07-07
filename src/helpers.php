7455efd39d1e092ce957e02e39b7cfb645c4a059
brunocfalcao <bruno.c.falcao@brunofalcao.me>
2019-07-08 01:53:13 +0200

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -67,3 +67,37 @@ if (! function_exists('date_range')) {
         return collect($dates);
     }
 }
+
+if (!function_exists('get_uri_collection_without_bindings')) {
+    /**
+     * Gets the uri collection without bindings.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    function get_uri_collection_without_bindings()
+    {
+        $uri = get_uri_collection();
+
+        // Remove { .. } items.
+        $segments = $uri->filter(function ($value, $key) {
+            return $value[0] != '{';
+        });
+
+        return $segments;
+    }
+}
+
+if (!function_exists('get_uri_collection')) {
+    /**
+     * Gets the uri collection (segments).
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    function get_uri_collection()
+    {
+        $router = app()->make('router');
+        $segments = collect(explode('/', $router->getCurrentRoute()->uri));
+
+        return $segments;
+    }
+}
